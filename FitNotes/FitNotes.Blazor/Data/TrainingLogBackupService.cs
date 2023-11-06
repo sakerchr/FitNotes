750b27f1b9314e0fd74dfbc8307d8e4e9973ca59
Sander Christiansen <sac@itverket.no>
2023-11-06 08:52:06 +0100

    From singleton to scoped

--- FitNotes/FitNotes.Blazor/Data/TrainingLogBackupService.cs
+++ FitNotes/FitNotes.Blazor/Data/TrainingLogBackupService.cs
@@ -6,12 +6,16 @@ namespace FitNotes.Blazor.Data
     public class TrainingLogBackupService
     {
         private static readonly string BackupFilePath = $"Data Source={Directory.GetCurrentDirectory()}/Data/backup.sqlite";
+        private readonly ILogger<TrainingLogBackupService> _logger;
         private TrainingLogBackup? _trainingLogBackup { get; set; }
+
+        public TrainingLogBackupService(ILogger<TrainingLogBackupService> logger) => _logger = logger;
 
         public async Task<TrainingLogBackup> GetTrainingLogBackupAsync()
         {
             if (_trainingLogBackup is not null) return _trainingLogBackup;
             var backup = await DownloadDataAsync();
+            _logger.LogDebug("Downloading data");
             _trainingLogBackup = backup;
             return backup;
         }
--- FitNotes/FitNotes.Blazor/Program.cs
+++ FitNotes/FitNotes.Blazor/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<TrainingLogBackupService>();
+builder.Services.AddScoped<TrainingLogBackupService>();
 
 var app = builder.Build();
 
